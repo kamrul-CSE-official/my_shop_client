20d93ad67f91cd07c2b237cd81e0f5a8ef3e1058
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2024-06-19 22:32:28 +0600

    initial setup

--- src/components/share/navbar.tsx
+++ src/components/share/navbar.tsx
@@ -1,11 +1,25 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
 
-const Navbar:FC = () => {
-    return (
-        <div>
-            Navbar
-        </div>
-    );
+const Navbar: FC = () => {
+  return (
+    <div>
+      <ul className="flex items-center justify-center gap-10">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/Login">Login</Link>
+        </li>
+        <li>
+          <Link to="/signup">Signup</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
