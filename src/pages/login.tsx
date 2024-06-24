7b782979d7e39d4cfefd53f76790dac76b7e5377
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2024-06-24 15:30:25 +0600

    update login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -13,10 +13,13 @@ import {
     FormMessage,
 } from '@/components/ui/form';
 import {Input} from '@/components/ui/input';
+import loginImage from '@/assets/Login.json';
+import LottieComponent from '@/components/share/lottieFile';
+import {Link} from 'react-router-dom';
 
 const loginSchema = z.object({
-    username: z.string().min(5, {
-        message: 'Username must be at least 5 characters.',
+    email: z.string().min(6, {
+        message: 'Email must be at least 6 characters.',
     }),
     password: z.string().min(6, {
         message: 'Password must be at least 6 characters.',
@@ -27,7 +30,7 @@ export default function LoginPage() {
     const LoginForm = useForm<z.infer<typeof loginSchema>>({
         resolver: zodResolver(loginSchema),
         defaultValues: {
-            username: '',
+            email: '',
             password: '',
         },
     });
@@ -37,55 +40,62 @@ export default function LoginPage() {
     }
 
     return (
-        <div className="flex min-h-screen items-center justify-center bg-gray-100 p-4">
-            <div className="max-w-md w-full bg-white rounded-lg shadow-lg p-8">
-                <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
-                <Form {...LoginForm}>
-                    <form
-                        onSubmit={LoginForm.handleSubmit(onSubmit)}
-                        className="space-y-6">
-                        <FormField
-                            control={LoginForm.control}
-                            name="username"
-                            render={({field}) => (
-                                <FormItem>
-                                    <FormLabel>Username</FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            placeholder="Enter your username"
-                                            {...field}
-                                            className="border-gray-300 w-full rounded-md p-2"
-                                        />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={LoginForm.control}
-                            name="password"
-                            render={({field}) => (
-                                <FormItem>
-                                    <FormLabel>Password</FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            type="password"
-                                            placeholder="••••••••"
-                                            {...field}
-                                            className="border-gray-300 w-full rounded-md p-2"
-                                        />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <Button
-                            type="submit"
-                            className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition">
-                            Login
-                        </Button>
-                    </form>
-                </Form>
+        <div className="min-w-screen min-h-screen flex flex-col items-center justify-center m-3 md:m-1">
+            <h2 className="text-4xl font-extrabold">Login</h2>
+            <p className="my-3 text-justify md:text-center">
+                Password must be at least 6 characters long, upto 25 characters
+                and include at least one uppercase letter, <br /> one lowercase
+                letter, one number, and one special character.
+            </p>
+            <div className="mx-3 md:mx-32 flex flex-col-reverse md:flex-row items-center justify-center">
+                <div className="w-full rounded-lg shadow-md p-1 md:p-8">
+                    <Form {...LoginForm}>
+                        <form
+                            onSubmit={LoginForm.handleSubmit(onSubmit)}
+                            className="space-y-6">
+                            <FormField
+                                control={LoginForm.control}
+                                name="email"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>Email</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                placeholder="Enter your email"
+                                                {...field}
+                                                className="border-gray-300 w-full rounded-md p-2"
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={LoginForm.control}
+                                name="password"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>Password</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                type="password"
+                                                placeholder="••••••••"
+                                                {...field}
+                                                className="border-gray-300 w-full rounded-md p-2"
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <Button type="submit">Login</Button>
+                        </form>
+                    </Form>
+                    <Link className="underline" to="/signup">
+                        Don't have an account? Signup
+                    </Link>
+                </div>
+                <LottieComponent animationData={loginImage} className="w-1/2" />
             </div>
         </div>
     );
